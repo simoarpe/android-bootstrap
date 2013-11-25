98ea21d12388a05fe1da91ab45464c1851018c31
sarpe <s.arpe@replyukltd.co.uk>
2013-11-25 15:05:01 +0000

    moved script into folder

---
+++ start-generation.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+	then echo "usage $0 \"AppName\" \"PackageName\""
+else
+	appName = $1
+	packageName = $2
+
+echo "copying script files"
+cp -Rf ./script/* ../
+cd ..
+node generate.js appName packageName
+fi
