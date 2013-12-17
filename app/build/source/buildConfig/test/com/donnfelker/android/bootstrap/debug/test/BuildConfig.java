5d0723d7fee9b1cc169556909d5f1eeca9bfbd08
sarpe <s.arpe@replyukltd.co.uk>
2013-12-17 10:31:14 +0000

    .gitignore is now working

---
+++ app/build/source/buildConfig/test/com/donnfelker/android/bootstrap/debug/test/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.donnfelker.android.bootstrap.debug.test;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
