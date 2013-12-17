3d5ea1726000fd05df6823cb81807cf4b1341792
sarpe <s.arpe@replyukltd.co.uk>
2013-12-17 09:59:09 +0000

    script fixed

--- start-generation.sh
+++ start-generation.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ $# -ne 2 ]
-	then echo "usage $0 \"AppName\" \"PackageName\""
+	then echo "usage $0 \"App Name\" \"PackageName\""
 else
-	appName = $1
-	packageName = $2
+	appName=$1
+	packageName=$2
 
 echo "copying script files"
 cp -Rf ./script/* ../
 cd ..
-node generate.js appName packageName
+node generate.js "$appName" "$packageName"
 fi
